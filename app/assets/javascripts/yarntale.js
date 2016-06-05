06e3c33a6c085df6ce53e083d82496a272cdc17b
Nazar Kuliyev <nazar.kuliev@gmail.com>
2016-06-05 14:26:59 +0600

    -

--- app/assets/javascripts/yarntale.js
+++ app/assets/javascripts/yarntale.js
@@ -1,16 +1,19 @@
 window.YARNTALE = {
     el: null, //hosting DOM element
     cur_slide_index: 0,
+    audio: null,
     slides: [], //array of objects {image,audio,captions}
 }
 
-
+YARNTALE.log = function(s) {
+    console.log("YARNTALE: "+s)
+}
 
 YARNTALE.attach_to = function(selector) {
     this.el = $(selector)
 
     this.el.append(
-      '<img class="slide"><div class="caption"></div><div class="timeline"></div>'
+      '<img class="slide"><div class="caption"></div><div class="timeline"></div><audio class="audio"></audio>'
     )
 
     var self = this;
@@ -26,16 +29,33 @@ YARNTALE.attach_to = function(selector) {
     $(document).on("click",".yarntale .timeline .slide",function() {
         YARNTALE.setSlideIndex($(this).data("index"))
     })
+
+    $(".yarntale .audio")[0].onended = function() {
+        YARNTALE.log('audio ended')
+        YARNTALE.next()
+    }
     return this;
 
 }
 
 
+YARNTALE.next = function() {
+    this.log("next")
+    if(this.cur_slide_index+1>=this.slides.length) {
+        this.log("tale ended")
+    } else {
+        this.setSlideIndex(this.cur_slide_index+1)
+    }
+}
+
 YARNTALE.setSlideIndex = function(i) {
+    this.log("set slide index")
     this.cur_slide_index = i;
 
     this.el.find("> .slide").attr('src',this.slides[i].image)
 
+    this.el.find(".audio").attr("src",this.slides[i].audio)
+    this.el.find(".audio")[0].play()
 }
 
 YARNTALE.start = function() {
