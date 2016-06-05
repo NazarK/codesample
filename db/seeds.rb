ae3d5872afca19ddd91cea8ba1f74b45225b9eeb
Nazar Kuliyev <nazar.kuliev@gmail.com>
2016-06-05 21:22:07 +0600

    sample tales

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 AdminUser.create!(email: 'admin@admin.com', password: '12345678')
+
+
+(1..2).each do |i|
+  tale = Tale.create name: "test tale #{i}"
+
+  (1..4).each do |slide_num|
+    j = 1 + (slide_num-1) % 3
+    tale.slides.create image: File.open("#{Rails.root}/public/sample_data/#{j}.png"), audio: File.open("#{Rails.root}/public/sample_data/#{j}.mp3"), caption: "slide number #{slide_num}"
+  end
+end
