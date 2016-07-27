8797daf6a0440825081a4ee08a7be2fdcd9fa1fb
Nazar Kuliyev <nazar.kuliev@gmail.com>
2016-07-27 10:59:59 +0600

    tale background audio volume adjust via control

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -43,42 +43,36 @@ function jquery_sortable_apply() {
 
 
 
+/* used in tales/:id/embed */
+var iframe_reload_timer;
+var code;
+function embed_generate() {
+  console.log('embed generate')
+  var w = $("#width").val()
+  var h = $("#height").val()
+  var id = $("#id").val()
+  code = "<iframe width='"+w+"' height='"+h+"' src='"+APP_HOST+"/t"+id+"/embed' frameborder='0' scrolling='no'></iframe>"
+  $("textarea#code").html(code)
+  clearTimeout(iframe_reload_timer)
+  iframe_reload_timer = setTimeout(function() {
+    $("#preview").html(code)
+    setTimeout(function() {
+      //$("iframe")[0].contentDocument.location.reload(true)
+    },200)
+  },1000)
+}
+
 /* ready function to be called on each load (and page:load turbolinks event) */
 var ready = function() {
   jquery_sortable_apply()
 
-
-  /* used in tales/:id/embed */
-  var iframe_reload_timer;
-  var code;
-  function embed_generate() {
-    console.log('embed generate')
-    var w = $("#width").val()
-    var h = $("#height").val()
-    var id = $("#id").val()
-    code = "<iframe width='"+w+"' height='"+h+"' src='"+APP_HOST+"/t"+id+"/embed' frameborder='0' scrolling='no'></iframe>"
-    $("textarea#code").html(code)
-    clearTimeout(iframe_reload_timer)
-    iframe_reload_timer = setTimeout(function() {
-      $("#preview").html(code)
-      setTimeout(function() {
-        //$("iframe")[0].contentDocument.location.reload(true)
-      },200)
-    },1000)
+  if($("body").is(".tales.embed")) {
+    embed_generate()
   }
 
-  $(document).on("change, keyup, input",".tales.embed #width",function() {
-    $("#height").val(Math.floor($("#width").val()*640/960))
-    embed_generate()
-  })
-
-  $(document).on("change, keyup, input",".tales.embed #height",function() {
-    $("#width").val(Math.floor($("#height").val()*960/640))
-    embed_generate()
-  })
-
-  if($("body").is(".tales, .embed")) {
-    embed_generate()
+  if($("body").is(".tales.edit")) {
+    console.log('setting tale audio volume')
+    $("#tale_audio_player")[0].volume = $("#tale_audio_volume").val()/100
   }
 
 };
@@ -86,6 +80,24 @@ var ready = function() {
 
 
 $(function() {
+
+
+    $(document).on("change, keyup, input",".tales.embed #width",function() {
+      $("#height").val(Math.floor($("#width").val()*640/960))
+      embed_generate()
+    })
+
+    $(document).on("change, keyup, input",".tales.embed #height",function() {
+      $("#width").val(Math.floor($("#height").val()*960/640))
+      embed_generate()
+    })
+
+
     ready();
     $(document).on('page:load', ready);
 });
+
+
+tale_audio_volume_change = function() {
+  $("#tale_audio_volume").val(Math.floor(this.volume*100))
+}
