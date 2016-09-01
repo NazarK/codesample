43dac3497cddc0f146f4343e42821cb3f300d5ce
Nazar Kuliyev <nazar.kuliev@gmail.com>
2016-09-01 12:00:34 +0600

    do deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
 # require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
-# require 'mina/rvm'    # for rvm support. (http://rvm.io)
+require 'mina/rvm'    # for rvm support. (http://rvm.io)
 
 # Basic settings:
 #   domain       - The hostname to SSH to.
@@ -10,20 +10,23 @@ require 'mina/git'
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
 
-set :domain, 'foobar.com'
-set :deploy_to, '/var/www/foobar.com'
-set :repository, 'git://...'
+set :domain, '139.59.211.66'
+set :deploy_to, '/var/www/yarntale'
+set :repository, 'git@bitbucket.org:nkcode/yarntale.git'
 set :branch, 'master'
 
 # For system-wide RVM install.
-#   set :rvm_path, '/usr/local/rvm/bin/rvm'
+set :rvm_path, '/usr/local/rvm/scripts/rvm'
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, ['config/database.yml', 'config/secrets.yml', 'log']
+set :shared_paths, ['config/database.yml', 'config/secrets.yml', 'log', 'config/application.yml', 'public/system', 'public/assets']
+
+set :launch_cmd, "cd #{deploy_to}/current && thin start -e production -p 8080 -d  --servers 1"
+set :shutdown_cmd, "ps ax | grep 808 | grep -v grep | awk '{print $1}' | xargs kill || true"
 
 # Optional settings:
-#   set :user, 'foobar'    # Username in the server to SSH to.
+set :user, 'root'    # Username in the server to SSH to.
 #   set :port, '30000'     # SSH port number.
 #   set :forward_agent, true     # SSH forward_agent.
 
@@ -35,7 +38,7 @@ task :environment do
   # invoke :'rbenv:load'
 
   # For those using RVM, use this to load an RVM version@gemset.
-  # invoke :'rvm:use[ruby-1.9.3-p125@default]'
+  invoke :'rvm:use[ruby-2.3.1@default]'
 end
 
 # Put any custom mkdir's in here for when `mina setup` is ran.
@@ -64,18 +67,48 @@ task :setup => :environment do
   end
 end
 
-desc "Deploys the current version to the server."
-task :deploy => :environment do
-  comment = ARGV[1] || '-'
-  system "git add ."
-  system "git commit -am \"#{comment}\""
-  system "git push origin master&"
-  system "git push prod master"
-  #system "heroku run rake db:migrate"
-end
 # For help in making your deploy script, see the Mina documentation:
 #
 #  - http://nadarei.co/mina
 #  - http://nadarei.co/mina/tasks
 #  - http://nadarei.co/mina/settings
 #  - http://nadarei.co/mina/helpers
+
+task :shell do
+  system "echo 'logging into shell on server'"
+  system "ssh #{user}@#{domain} -t \"cd #{deploy_to}/current; bash --login\""
+end
+
+desc "Deploys the current version to the server."
+task :deploy => :environment do
+  to :before_hook do
+    invoke :push
+  end
+
+  deploy do
+    # Put things that will set up an empty directory into a fully set-up
+    # instance of your project.
+    invoke :'git:clone'
+    invoke :'deploy:link_shared_paths'
+    invoke :'bundle:install'
+    invoke :'rails:db_migrate'
+    invoke :'rails:assets_precompile'
+    invoke :'deploy:cleanup'
+    queue "cd #{deploy_to}/current ; RAILS_ENV=production bin/delayed_job stop"
+
+    to :launch do
+      queue shutdown_cmd
+      queue launch_cmd
+      queue "cd #{deploy_to}/current ; RAILS_ENV=production bin/delayed_job start"
+    end
+  end
+end
+
+task :push do
+  # Put things to run locally before ssh
+  comment = ENV['m'] || '-'
+  puts "commit comment: #{comment}"
+  system "git add ."
+  system "git commit -am \"#{comment}\""
+  system "git push origin master"  
+end
