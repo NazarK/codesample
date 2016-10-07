e5e4b7252fe0b61fa2d4191d7471599233825362
Nazar Kuliyev <nazar.kuliev@gmail.com>
2016-10-07 09:37:15 +0600

    admin

--- app/admin/slide.rb
+++ app/admin/slide.rb
@@ -1,49 +1,20 @@
 ActiveAdmin.register Slide do
   menu false
-
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
-
   index do
-    selectable_column
-    id_column
-    column :tale
-    column :image do |r|
-      link_to image_tag(r.image.url(:thumb)), r.image.url(:original), target: "_blank"
-    end
-    column :image_size do |r|
-      (r.image_file_size.to_f / 1000).to_i.to_s+"kb"
-    end
-    column :audio do |r|
-      link_to r.audio_file_name, r.audio.url, target: "_blank"
-    end
-    column :audio_size do |r|
-      (r.audio_file_size.to_f / 1000).to_i.to_s+"kb"
-    end
+    column :id
     column :caption
-    actions
-  end
-
-  form do |f|
-    f.inputs "" do
-      f.input :tale
-      f.input :image, as: :file
-      f.input :audio, as: :file
-      f.input :caption
+    column :media do |slide|
+      if slide.video.present?
+        span video_tag slide.video.url, controls: true, style: "width:200px"
+      end  
+      if slide.image.present?
+        span image_tag slide.image.url, style: "width:200px"
+      end  
     end
-    f.actions
+    column :audio do |slide|
+      if slide.audio.present?
+        span audio_tag slide.audio.url, controls: true
+      end  
+    end  
   end
-
-
-
 end
