a80cb5774d5abe00dff86330d56f7c0f723bb89f
Nazar Kuliyev <nazar.kuliev@gmail.com>
2016-11-30 16:37:18 +0600

    app name

---
+++ meteor-app/mobile-config.js
@@ -0,0 +1,8 @@
+App.info({
+  id: 'com.yarntale.app',
+  name: 'YarnTale',
+  description: 'YarnTale - create online slide show',
+  author: 'author',
+  email: 'dev@yarntale.com',
+  website: 'http://yarntale.cloudspaint.com'
+});
