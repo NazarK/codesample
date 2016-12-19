26311646ac119fd8d2f2777a1ee967f1e5b8d6d6
Nazar Kuliyev <nazar@Nazars-iMac.local>
2016-12-19 13:13:28 +0600

    ajax dimm, record progress

--- meteor-app/client/components/slide_edit.js.jsx
+++ meteor-app/client/components/slide_edit.js.jsx
@@ -24,11 +24,43 @@ export default class MobileSlideEdit extends React.Component {
 
   }
 
+
+
   componentDidMount() {
     console.log("slide component did mount")
     this.refs.audio.value=''
     this.refs.video.value=''
     this.refs.image.value=''
+    console.log("audio input events")
+    window.addEventListener('audioinput', (evt) => {
+      try {
+          if (evt && evt.data) {
+            let max = Math.max.apply(null,evt.data)
+            max = Math.abs(max)
+            console.log("data received: ", evt.data.length, " max:",max)
+            this.totalReceivedData += evt.data.length;
+            this.lastMax = max;
+
+            this.audioDataBuffer = this.audioDataBuffer.concat(evt.data);
+
+
+          }
+      }
+      catch (ex) {
+          console.log("onAudioInputCapture ex: " + ex);
+      }
+    }, false);
+
+  }
+
+  force_stop_rec() {
+    if (window.audioinput && audioinput.isCapturing()) {
+      this.stop()
+    }
+  }
+
+  componentWillUnmount() {
+    this.force_stop_rec()
   }
 
   componentWillMount() {
@@ -52,9 +84,10 @@ export default class MobileSlideEdit extends React.Component {
 
   form_submit(event) {
     event.preventDefault()
+    this.force_stop_rec()
     var new_record = !this.state.id
 
-    form = $(event.currentTarget)    
+    form = $(event.currentTarget)
     var formData = new FormData(form[0])
     formData.append("user_email", localStorage['user_email'])
     formData.append("user_token", localStorage['user_token'])
@@ -63,7 +96,7 @@ export default class MobileSlideEdit extends React.Component {
       console.log("got recorded audio blob, adding to form data")
       formData.append("slide[audio]",this.state.recorded_audio_blob,"audio.wav")
     }
-        
+
     $.ajax({
         url: form.attr("action"),
         data: formData,
@@ -73,30 +106,40 @@ export default class MobileSlideEdit extends React.Component {
     }).done( (resp)=>{
         console.log("submitted",resp)
         this.setState({recorded_audio_blob: null})
-        this.setState(resp)        
+        this.setState(resp)
         $(".got-file").removeClass("got-file")
         //WAS NEW RECORD
         if(new_record)
           window.history.replaceState('','',`/m/slides/${resp.id}/edit`)
     })
-    
+
     return false;
   }
 
   back() {
     this.props.router.goBack()
   }
-  
+
   record(e) {
     console.log("record")
     $(this.refs.record_btn).addClass("got-file").hide()
-    $(this.refs.stop_record_btn).addClass("got-file").show()    
-    e.preventDefault()      
+    $(this.refs.stop_record_btn).addClass("got-file").show()
+    $(this.refs.progress).addClass("got-file")
+    e.preventDefault()
     try {
         if (window.audioinput) {
             if (!audioinput.isCapturing()) {
-                // Start with default values and let the plugin handle conversion from raw data to web audio                
-                audioinput.start({ streamToWebAudio: true });                
+                // Start with default values and let the plugin handle conversion from raw data to web audio
+                this.captureCfg = {
+                    sampleRate: 44100,
+                    bufferSize: 4096,
+                    channels: 1,
+                    format: "PCM_16BIT",
+                    audioSourceType: 0
+                };
+
+                // Start with default values and let the plugin handle conversion from raw data to web audio
+                audioinput.start({ streamToWebAudio: true });
                 window.audioRecorder = new WebAudioRecorder(audioinput,{workerDir: "/"})
                 console.log("created window.audioRecorder", window.audioRecorder)
 
@@ -109,17 +152,23 @@ export default class MobileSlideEdit extends React.Component {
 
                 window.audioRecorder.startRecording()
                 console.log("Capturing audio!");
+
+                this.progressUpdate = setInterval(() => {
+                    $("#progress #time").html(Math.round(10*window.audioRecorder.recordingTime())/10+"s")
+                    //$("#progress #level").css({width: Math.round(100*this.lastMax)+"%"})
+                }, 300);
+
             }  else {
                 alert("Already capturing!");
             }
         }
     } catch (ex) {
         alert("startCapture exception: " + ex);
-    }  
+    }
   }
-  
+
   stop(e) {
-    e.preventDefault()
+    e && e.preventDefault()
     console.log("stop")
     $(this.refs.stop_record_btn).hide()
     $(this.refs.record_btn).show()
@@ -132,19 +181,21 @@ export default class MobileSlideEdit extends React.Component {
       this.setState({recorded_audio_blob: blob})
       var url = URL.createObjectURL(blob);
       this.setState({recorded_audio_url: url})
-    //device env  
+    //device env
     } else {
       if (window.audioinput && audioinput.isCapturing()) {
           audioRecorder.finishRecording()
           audioinput.stop();
-      }      
+          clearInterval(this.progressUpdate)
+
+      }
     }
   }
-  
+
   file_chosen(e) {
     $(e.currentTarget).parents(".btn").addClass("got-file")
   }
-  
+
   render() {
 
     console.log("slide component render", this.state)
@@ -192,29 +243,33 @@ export default class MobileSlideEdit extends React.Component {
                 <MobileSlidePreview slide={this.state} />
               </label>
               <label className="item item-input item-stacked-label">
-                <span className="input-label float-left">Image</span>
-                <span className="btn btn-default btn-file float-left" style={{margin:"5px 10px 5px 0px"}}>                
+                <span className="input-label short-label float-left vertical-middle-label">Image</span>
+                <span className="btn btn-default btn-file float-left" style={{margin:"5px 10px 5px 0px"}}>
                   Choose File <input type="file" onChange={this.file_chosen.bind(this)} ref="image" accept="image/*;capture=camera" name="slide[image]"/>
                 </span>
               </label>
               <label className="item item-input item-stacked-label">
-                <div className="input-label float-left" >Audio</div>
-                <span className="btn btn-default btn-file float-left" style={{margin:"5px 10px 5px 0px"}}>                
+                <div className="input-label short-label float-left  vertical-middle-label">Audio</div>
+                <span className="btn btn-default btn-file float-left" style={{margin:"5px 10px 5px 0px"}}>
                   Choose File <input type="file" ref="audio" onChange={this.file_chosen.bind(this)}  name="slide[audio]" accept="audio/*;capture=microphone" style={{float:"left"}}/>
-                </span>              
-                <div ref="record_btn" className="btn btn-default float-left"  onClick={this.record.bind(this)} style={{width:"60px", textAlign: "center" }}>
+                </span>
+                <div ref="record_btn" className="btn btn-default float-left"  onClick={this.record.bind(this)} style={{width:"50px", textAlign: "center" }}>
                   <i  className='fa fa-microphone fa-2x' style={{color:"red" }}></i>
                 </div>
-                <div ref="stop_record_btn" className="btn btn-default float-left" onClick={this.stop.bind(this)} style={{width:"60px", textAlign: "center", display: "none"}}>
+                <div ref="stop_record_btn" className="btn btn-default float-left" onClick={this.stop.bind(this)} style={{width:"50px", textAlign: "center", display: "none"}}>
                   <i  className='fa fa-stop-circle-o fa-2x' style={{color:"red" }}></i>
                 </div>
-                <div className="float-left" ref="recorded_time">                  
+                <div className="float-left" ref="progress" id="progress" style={{marginLeft:"10px", height:"40px", position: "relative", textAlign: "center", display: "none"}}>
+                  <div id="level_wrap" style={{width:"50px",height:"8px",position:"relative",border:"1px solid #aaa", marginTop: "10px", display: "none"}}>
+                    <div id="level" style={{backgroundColor:"red",width:"0%",position:"relative",height:"100%"}}></div>
+                  </div>
+                  <div id="time"></div>
                 </div>
               </label>
               <label className="item item-input item-stacked-label">
-                <span className="input-label float-left">Video</span>
+                <span className="input-label short-label float-left  vertical-middle-label" >Video</span>
 
-                <span className="btn btn-default btn-file float-left" style={{margin:"5px 10px 5px 0px"}}>                
+                <span className="btn btn-default btn-file float-left" style={{margin:"5px 10px 5px 0px"}}>
                   Choose File <input type="file"  ref="video" accept="video/*;capture=camera" onChange={this.file_chosen.bind(this)}  name="slide[video]"/>
                 </span>
               </label>
