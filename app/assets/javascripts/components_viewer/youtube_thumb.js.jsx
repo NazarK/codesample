a335c363565737eafa1dbfba86ba58ed56129187
Nazar Kuliyev <nazar.kuliev@gmail.com>
2017-02-13 18:58:29 +0600

    editor layout fix

--- app/assets/javascripts/components_viewer/youtube_thumb.js.jsx
+++ app/assets/javascripts/components_viewer/youtube_thumb.js.jsx
@@ -1,8 +1,13 @@
 class YoutubeThumb extends React.Component {
-  
+
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
   render() {
     return (
-      <img className={'youtube_thumb'+(this.props.dimmed ? ' dimmed' : '')} src={`http://img.youtube.com/vi/${this.props.videoId}/0.jpg`}></img>
+       <img className={'youtube_thumb'+(this.props.dimmed ? ' dimmed' : '')} src={`http://img.youtube.com/vi/${this.props.videoId}/0.jpg`}></img>
     )
   }
 }
