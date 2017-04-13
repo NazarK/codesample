35f09203a3c2ff56e9e3b206b02002878defcf46
Nazar Kuliyev <nazar.kuliev@gmail.com>
2017-04-13 19:46:33 +0600

    admin login right redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include ActiveDevice
   skip_before_filter :set_mobile_format
-  
+
   def after_sign_in_path_for(resource)
     if is_mobile_browser?
       "/m"
     else
-      "/"
+      if resource.is_a? AdminUser
+        "/admin"
+      else
+        "/"
+      end
     end
   end
 
-    
+
 end
